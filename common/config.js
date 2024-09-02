9105ab0d340a2f8ea0effcbca728887c1b93ae38
Sanyamverma2004 <sanyam4590@gmail.com>
2024-09-02 12:18:02 +0530

    Replaced Client And Server URLs with Hosted Ones - Ttest 1

--- common/config.js
+++ common/config.js
@@ -1,4 +1,6 @@
 const SERVER_PORT = 3000;
 const CLIENT_PORT = 5173;
-export const SERVER_URL = `http://localhost:${SERVER_PORT}`;
-export const CLIENT_URL = `http://localhost:${CLIENT_PORT}`;
+// export const SERVER_URL = `http://localhost:${SERVER_PORT}`;
+// export const CLIENT_URL = `http://localhost:${CLIENT_PORT}`;
+export const CLIENT_URL = `https://pitch-perfect-mu.vercel.app`;
+export const SERVER_URL = `https://pitch-perfect-1f0m.onrender.com`;
